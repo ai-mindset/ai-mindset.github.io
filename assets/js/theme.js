51fb79e210ec350cf85163e0ee74dba2e9798685
Eirini <49712302+ai-mindset@users.noreply.github.com>
2024-11-17 11:29:34 +0000

    fix: dark theme code blocks

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -5,3 +5,12 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("theme-toggle").innerHTML = "☀️ Light";
     }
 });
+
+function toggleTheme() {
+    const body = document.querySelector("body");
+    const button = document.getElementById("theme-toggle");
+    const isDark = body.classList.toggle("dark-theme");
+
+    button.innerHTML = isDark ? "☀️ Light" : "🌙 Dark'"
+    localStorage.setItem("darkTheme", isDark);
+}
